308aec63ef75de1fd65d359cd54b729e4418cb56
123tbone321 <123tbone321@gmail.com>
2020-11-16 19:50:04 -0500

    refactoring

--- MyceliumProject/src/scripts/CollisionDetection.js
+++ MyceliumProject/src/scripts/CollisionDetection.js
@@ -4,14 +4,13 @@ function collisionBranchBranch(b1, b2) {
     if ((b1.p1.x === b1.p2.x && b1.p1.y === b1.p2.y) || (b2.p1.x === b2.p2.x && b2.p1.y === b2.p2.y)) {
         return false
     }
-
+    
     denominator = ((b2.p2.y - b2.p1.y) * (b1.p2.x - b1.p1.x) - (b2.p2.x - b2.p1.x) * (b1.p2.y - b1.p1.y))
 
   // Lines are parallel
     if (denominator === 0) {
         return false
     }
-
     let ua = ((b2.p2.x - b2.p1.x) * (b1.p1.y - b2.p1.y) - (b2.p2.y - b2.p1.y) * (b1.p1.x - b2.p1.x)) / denominator
     let ub = ((b1.p2.x - b1.p1.x) * (b1.p1.y - b2.p1.y) - (b1.p2.y - b1.p1.y) * (b1.p1.x - b2.p1.x)) / denominator
 
@@ -20,10 +19,9 @@ function collisionBranchBranch(b1, b2) {
         return false
     }
 
-  // Return an object with the x and y coordinates of the intersection
+  // Return a object with the x and y coordinates of the intersection
     let x = b1.p1.x + ua * (b1.p2.x - b1.p1.x)
     let y = b1.p1.y + ua * (b1.p2.y - b1.p1.y)
-
     return {x, y}
 }
 
@@ -42,15 +40,15 @@ function collisionBranchFoodNode(branch, foodNode){
     if(isNaN(d)){ // no intercept
         return false;
     }
-    else { // intercept found, further test the branch to see if it's within the segment
-      u1 = (b - d) / c;  // these represent the unit distance of point one and two on the branch
+    else { // intercept found, further test the line to see if it's within the segment
+      u1 = (b - d) / c;  // these represent the unit distance of point one and two on the lines
       u2 = (b + d) / c;    
       intersectionPoint = {};   // return point
-      if(u1 <= 1 && u1 >= 0){  // add point if on the branch segment
+      if(u1 <= 1 && u1 >= 0){  // add point if on the line segment
           intersectionPoint.x = branch.p1.x + v1.x * u1;
           intersectionPoint.y = branch.p1.y + v1.y * u1;
       }
       if ($.isEmptyObject(intersectionPoint)) return false; // none found on the segment either
       return intersectionPoint;
-    }
+    } // Return a object with the x and y coordinates of the intersection
 }
