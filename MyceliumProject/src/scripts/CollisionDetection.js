c01b7371d3b8091ebd695747012d3ba29765e862
123tbone321 <123tbone321@gmail.com>
2020-11-16 19:08:13 -0500

    naming conventions..

--- MyceliumProject/src/scripts/CollisionDetection.js
+++ MyceliumProject/src/scripts/CollisionDetection.js
@@ -1,16 +1,4 @@
-/*function collisionBranchBranch(b1, b2) { // Branch 1 and Branch 2 ([a,b], [c,d]) | ([p,q], [r,s])
-	var det, gamma, lambda;
-	det = (b1.p2.x - b1.p1.x) * (b2.p2.y - b2.p1.y) - (b2.p2.x - b2.p1.x) * (b1.p2.y - b1.p1.y);
-	if (det === 0) return false;
-	else {
-		lambda = ((b2.p2.y - b2.p1.y) * (b2.p2.x - b1.p1.x) + (b2.p1.x - b2.p2.x) * (b2.p2.y - b1.p1.y)) / det;
-		gamma = ((b1.p1.y - b1.p2.y) * (b2.p2.x - b1.p1.x) + (b1.p2.x - b1.p1.x) * (b2.p2.y - b1.p1.y)) / det;
-		return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
-	}
-}
-*/
-function intersect(b1, b2) {
-  // b1.p1.x, b1.p1.y, b1.p2.x, b1.p2.y, b2.p1.x, b2.p1.y, b2.p2.x, b2.p2.y
+function collisionBranchBranch(b1, b2) {
 
   // Check if none of the lines are of length 0
     if ((b1.p1.x === b1.p2.x && b1.p1.y === b1.p2.y) || (b2.p1.x === b2.p2.x && b2.p1.y === b2.p2.y)) {
@@ -32,35 +20,35 @@ function intersect(b1, b2) {
         return false
     }
 
-  // Return a object with the x and y coordinates of the intersection
+  // Return an object with the x and y coordinates of the intersection
     let x = b1.p1.x + ua * (b1.p2.x - b1.p1.x)
     let y = b1.p1.y + ua * (b1.p2.y - b1.p1.y)
 
     return {x, y}
 }
 
-function collisionBranchFoodNode(circle, line){
+function collisionBranchFoodNode(branch, foodNode){
     var b, c, d, u1, intersectionPoint, v1, v2;
     v1 = {};
     v2 = {};
-    v1.x = line.p2.x - line.p1.x;
-    v1.y = line.p2.y - line.p1.y;
-    v2.x = line.p1.x - circle.p.x;
-    v2.y = line.p1.y - circle.p.y;
+    v1.x = branch.p2.x - branch.p1.x;
+    v1.y = branch.p2.y - branch.p1.y;
+    v2.x = branch.p1.x - foodNode.p.x;
+    v2.y = branch.p1.y - foodNode.p.y;
     b = (v1.x * v2.x + v1.y * v2.y);
     c = 2 * (v1.x * v1.x + v1.y * v1.y);
     b *= -2;
-    d = Math.sqrt(b * b - 2 * c * (v2.x * v2.x + v2.y * v2.y - circle.radius * circle.radius));
+    d = Math.sqrt(b * b - 2 * c * (v2.x * v2.x + v2.y * v2.y - foodNode.radius * foodNode.radius));
     if(isNaN(d)){ // no intercept
         return false;
     }
-    else { // intercept found, further test the line to see if it's within the segment
-      u1 = (b - d) / c;  // these represent the unit distance of point one and two on the line
+    else { // intercept found, further test the branch to see if it's within the segment
+      u1 = (b - d) / c;  // these represent the unit distance of point one and two on the branch
       u2 = (b + d) / c;    
       intersectionPoint = {};   // return point
-      if(u1 <= 1 && u1 >= 0){  // add point if on the line segment
-          intersectionPoint.x = line.p1.x + v1.x * u1;
-          intersectionPoint.y = line.p1.y + v1.y * u1;
+      if(u1 <= 1 && u1 >= 0){  // add point if on the branch segment
+          intersectionPoint.x = branch.p1.x + v1.x * u1;
+          intersectionPoint.y = branch.p1.y + v1.y * u1;
       }
       if ($.isEmptyObject(intersectionPoint)) return false; // none found on the segment either
       return intersectionPoint;
